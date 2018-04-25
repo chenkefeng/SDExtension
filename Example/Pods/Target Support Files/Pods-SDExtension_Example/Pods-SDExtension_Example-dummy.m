6bcc7f9767c80a3a7e81793b4bec394f9264b64d
陈克锋 <chenkefeng@kuaicto.com>
2018-04-25 22:27:14 +0800

    初始化

---
+++ Example/Pods/Target Support Files/Pods-SDExtension_Example/Pods-SDExtension_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SDExtension_Example : NSObject
+@end
+@implementation PodsDummy_Pods_SDExtension_Example
+@end
---
+++ Example/Pods/Target Support Files/SDExtension/SDExtension-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SDExtension : NSObject
+@end
+@implementation PodsDummy_SDExtension
+@end
